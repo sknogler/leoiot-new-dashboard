d5c65c7a332a298b3778b51eb20bfb90bf299e80
Simon Knogler <simon.knogler@liwest.at>
2021-04-22 21:51:40 +0200

    recieving mqtt data added routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {MqttService} from 'ngx-mqtt';
+import {MQTT_SERVICE_OPTIONS} from './app.module';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'dashboard';
+  topic = 'og/107/noise/state';
+  value: string;
+
+  constructor(private mqttService: MqttService) {}
+
+  ngOnInit(): void {
+    console.log('Hello Word');
+
+    this.mqttService.connect(MQTT_SERVICE_OPTIONS);
+
+    setTimeout(() => {
+      this.mqttService.unsafePublish(this.topic, 'gsicht');
+    }, 1000);
+
+    this.mqttService.observe(this.topic).subscribe(data => {
+      console.log(data.topic + ':');
+      console.log()
+      this.value = data.payload.toLocaleString().split(',')[0];
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,35 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import {IMqttServiceOptions, MqttModule} from 'ngx-mqtt';
+
+import { Og107Component } from './og107/og107.component';
+import { Ugk03Component } from './ugk03/ugk03.component';
+import { Ugk04Component } from './ugk04/ugk04.component';
+
+export const MQTT_SERVICE_OPTIONS: IMqttServiceOptions = {
+  hostname: 'localhost',
+  port: 9001,
+  protocol: 'ws',
+};
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Og107Component,
+    Ugk03Component,
+    Ugk04Component,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserModule,
+    MqttModule.forRoot(MQTT_SERVICE_OPTIONS),
+    RouterModule.forRoot([
+      {path: 'og107', component: Og107Component},
+      {path: 'ugk03', component: Ugk03Component},
+      {path: 'ugk04', component: Ugk04Component},
+    ]),
   ],
   providers: [],
   bootstrap: [AppComponent]
