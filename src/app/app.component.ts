f3ecc49b68a92152fe78a480871b6af838768cd9
Simon Knogler <simon.knogler@liwest.at>
2021-04-22 22:21:42 +0200

    trying to recieve different values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,8 +9,12 @@ import {MQTT_SERVICE_OPTIONS} from './app.module';
 })
 export class AppComponent implements OnInit{
   title = 'dashboard';
-  topic = 'og/107/noise/state';
-  value: string;
+  noiseTopic = 'og/107/noise/state';
+  tempTopic = 'og/107/temperature/state';
+  humidTopic = 'og/107/humidity/state';
+  noiseValue: string;
+  tempValue: string;
+  humidValue: string;
 
   constructor(private mqttService: MqttService) {}
 
@@ -20,13 +24,30 @@ export class AppComponent implements OnInit{
     this.mqttService.connect(MQTT_SERVICE_OPTIONS);
 
     setTimeout(() => {
-      this.mqttService.unsafePublish(this.topic, 'gsicht');
+      this.mqttService.unsafePublish(this.noiseTopic, 'unsafePublish');
+      this.mqttService.unsafePublish(this.tempTopic, 'unsafePublish');
+      this.mqttService.unsafePublish(this.humidTopic, 'unsafePublish');
     }, 1000);
 
-    this.mqttService.observe(this.topic).subscribe(data => {
+    // Noise observe
+    this.mqttService.observe(this.noiseTopic).subscribe(data => {
       console.log(data.topic + ':');
-      console.log()
-      this.value = data.payload.toLocaleString().split(',')[0];
+      this.noiseValue = data.payload.toLocaleString().split(',')[0];
+      console.log(this.noiseValue);
+    });
+
+    // Temperature observe
+    this.mqttService.observe(this.tempTopic).subscribe(data => {
+      console.log(data.topic + ':');
+      this.tempValue = data.payload.toLocaleString().split(',')[0];
+      console.log(this.tempValue);
+    });
+
+    // Humidity observe
+    this.mqttService.observe(this.humidTopic).subscribe(data => {
+      console.log(data.topic + ':');
+      this.humidValue = data.payload.toLocaleString().split(',')[0];
+      console.log(this.humidValue);
     });
   }
 }
